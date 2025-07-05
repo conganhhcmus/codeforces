f103a619d4a11d181263fa3ee589f1aa659c2348
conganhhcmus <conganhhcmus@gmail.com>
2025-07-05 15:43:53 +0700

    feat: adjust solution of problem 2117F

--- 2117F.py
+++ 2117F.py
@@ -33,9 +33,32 @@ def YN(a):
 INF = 10**18
 MOD = 10**9 + 7
 
+lens = []
+lca = -1
+
+
+def dfs(u, p, adj, dep):
+    if len(adj[u]) > 2:
+        global lca
+        lca = dep
+
+    leaf = True
+    for v in adj[u]:
+        if v != p:
+            dfs(v, u, adj, dep + 1)
+            leaf = False
+
+    if leaf:
+        global lens
+        lens.append(dep)
+
 
 # Main function
 def solve():
+    global lca
+    lca = -1
+    global lens
+    lens = []
     n = read_int()
     adj = [[] for _ in range(n + 1)]
     for _ in range(n - 1):
@@ -43,7 +66,26 @@ def solve():
         adj[u].append(v)
         adj[v].append(u)
 
-    ans = 0
+    adj[1].append(0)  # dummy node
+    dfs(1, 0, adj, 1)
+
+    pw = [1] * (n + 1)
+    for i in range(1, n + 1):
+        pw[i] = (pw[i - 1] * 2) % MOD
+
+    if len(lens) > 2:
+        return print(0)
+
+    if len(lens) == 1:
+        return print(pw[n])
+    diff = abs(lens[0] - lens[1])
+    x = diff + lca
+    ans = 1
+    if diff == 0:
+        ans = 2 * pw[x] % MOD
+    else:
+        ans = ans * (pw[x] + pw[x - 1]) % MOD
+
     print(ans)
 
 
