5e8c0fe4b639a9fcc7f5aa9e978807a64f9a9a67
Le Doan Cong Anh <conganhhcmus@gmail.com>
2026-05-12 22:57:35 +0700

    feat: add initial implementation of problem 1526C2

---
+++ 1526C2.py
@@ -0,0 +1,57 @@
+import heapq
+import sys
+
+# Fast I/O
+input = sys.stdin.readline
+
+
+# Recursion Limit
+sys.setrecursionlimit(10**6)
+
+
+# Input/Output Helpers
+def read_int():
+    return int(input())
+
+
+def read_ints():
+    return list(map(int, input().split()))
+
+
+def read_str():
+    return input().strip()
+
+
+def read_strs():
+    return list(input().split())
+
+
+def YN(a):
+    print("YES" if a else "NO")
+
+
+# Constants
+INF = 10**18
+MOD = 10**9 + 7
+
+
+# Main function
+def solve():
+    n = read_int()
+    arr = read_ints()
+    ans = 0
+    pq = []
+    s = 0
+    for i in range(n):
+        s += arr[i]
+        heapq.heappush(pq, arr[i])
+        while s < 0:
+            s -= heapq.heappop(pq)
+        ans = max(ans, len(pq))
+
+    print(ans)
+
+
+# Entry point
+if __name__ == "__main__":
+    solve()
